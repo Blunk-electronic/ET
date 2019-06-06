19c24e3bf40e1c784aed86ba0cdfe8d49479f980
Mario Blunk <mario.blunk@blunk-electronic.de>
2019-06-06 12:18:07 +0200

    rename function equal_reference to equal_name

--- src/lib/et_libraries.adb
+++ src/lib/et_libraries.adb
@@ -848,7 +848,7 @@ package body et_libraries is
 		return result;
 	end;	
 
-	function equal_reference (left, right : in type_device_name) return boolean is
+	function equal_name (left, right : in type_device_name) return boolean is
 	-- Returns true if left equals right.
 	-- Example: if IC4 = IC4 then return true.
 		result : boolean := false;
--- src/lib/et_libraries.ads
+++ src/lib/et_libraries.ads
@@ -439,7 +439,7 @@ package et_libraries is
 	-- Returns true if left comes before right.
 	-- If left equals right, the return is false.	
 
-	function equal_reference (left, right : in type_device_name) return boolean;
+	function equal_name (left, right : in type_device_name) return boolean;
 	-- Returns true if left equals right.
 	
 	function to_string (name : in type_device_name) return string;
