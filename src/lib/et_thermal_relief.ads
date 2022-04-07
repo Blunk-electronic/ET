16c58d10c0e8e32c783fc94fa9f5b975ac0acbb0
Mario Blunk <mario.blunk@blunk-electronic.de>
2022-04-07 11:02:10 +0200

    prepare package et_thermal_relief

---
+++ src/lib/et_thermal_relief.ads
@@ -0,0 +1,85 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                             SYSTEM ET                                    --
+--                                                                          --
+--                           THERMAL RELIEF                                 --
+--                                                                          --
+--                              S p e c                                     --
+--                                                                          --
+--         Copyright (C) 2017 - 2022 Mario Blunk, Blunk electronic          --
+--                                                                          --
+--    This program is free software: you can redistribute it and/or modify  --
+--    it under the terms of the GNU General Public License as published by  --
+--    the Free Software Foundation, either version 3 of the License, or     --
+--    (at your option) any later version.                                   --
+--                                                                          --
+--    This program is distributed in the hope that it will be useful,       --
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of        --
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         --
+--    GNU General Public License for more details.                          --
+--                                                                          --
+--    You should have received a copy of the GNU General Public License     --
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>. --
+------------------------------------------------------------------------------
+
+--   For correct displaying set tab width in your edtior to 4.
+
+--   The two letters "CS" indicate a "construction site" where things are not
+--   finished yet or intended for the future.
+
+--   Please send your questions and comments to:
+--
+--   info@blunk-electronic.de
+--   or visit <http://www.blunk-electronic.de> for more contact data
+--
+--   history of changes:
+--
+--   to do:
+
+
+with ada.text_io;				use ada.text_io;
+with ada.characters;			use ada.characters;
+with ada.characters.latin_1;	use ada.characters.latin_1;
+with ada.characters.handling;	use ada.characters.handling;
+
+with ada.strings.maps;			use ada.strings.maps;
+with ada.strings.bounded; 		use ada.strings.bounded;
+with ada.containers; 			use ada.containers;
+
+with ada.containers.doubly_linked_lists;
+with ada.containers.indefinite_doubly_linked_lists;
+with ada.containers.ordered_maps;
+with ada.containers.indefinite_ordered_maps;
+with ada.containers.ordered_sets;
+
+with et_general;
+with et_string_processing;		use et_string_processing;
+
+with et_pcb_coordinates;		use et_pcb_coordinates;
+with et_geometry;				use et_geometry;
+with et_board_shapes_and_text;	use et_board_shapes_and_text;
+with et_design_rules;			use et_design_rules;
+
+
+package et_thermal_relief is
+	
+	use pac_geometry_brd;
+	use pac_geometry_2;
+	--use pac_contours;
+	--use pac_polygons;
+
+	
+	
+	
+	
+
+														 
+	
+end et_thermal_relief;
+
+-- Soli Deo Gloria
+
+-- For God so loved the world that he gave 
+-- his one and only Son, that whoever believes in him 
+-- shall not perish but have eternal life.
+-- The Bible, John 3.16
