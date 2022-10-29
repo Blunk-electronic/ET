1cf6ae37f483f3920d6bba98620ebb9366b1f26a
Mario Blunk <mario.blunk@blunk-electronic.de>
2022-10-29 08:19:12 +0200

    undo useless update of boundaries. The borders are offset by the caller afterward

--- src/lib/et_text.adb
+++ src/lib/et_text.adb
@@ -613,7 +613,6 @@ package body et_text is
 					scale_and_move_border (border_vertices);
 					p_scratch := to_polygon (border_vertices);
 					offset_polygon (p_scratch, half_line_width);
-					update_boundaries (p_scratch);
 					result.borders.append (p_scratch);
 				end if;
 			end add;
