307b11594b922fb0c2a04dfee2b8a33e7355f439
Mario Blunk <mario.blunk@blunk-electronic.de>
2022-11-23 18:37:47 +0100

    add debug messages

--- src/lib/et_gui.adb
+++ src/lib/et_gui.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                               B o d y                                    --
 --                                                                          --
---         Copyright (C) 2017 - 2021 Mario Blunk, Blunk electronic          --
+--         Copyright (C) 2017 - 2022 Mario Blunk, Blunk electronic          --
 --                                                                          --
 --    This program is free software: you can redistribute it and/or modify  --
 --    it under the terms of the GNU General Public License as published by  --
@@ -46,8 +46,8 @@ package body et_gui is
 		project			: in pac_project_name.bounded_string;	-- blood_sample_analyzer
 		module			: in pac_generic_modules.cursor; -- cursor of generic module to be edited
 		sheet			: in et_coordinates.type_sheet := et_coordinates.type_sheet'first; -- the sheet to be opened
-		log_threshold_in: in type_log_level) is
-
+		log_threshold_in: in type_log_level) 
+	is
 		use et_canvas_schematic;
 		use et_canvas_schematic.pac_canvas;
 		
@@ -56,14 +56,23 @@ package body et_gui is
 		-- using the gui wide variable log_threshold:
 		log_threshold := log_threshold_in;
 
-		-- Set the current project name:
-		current_active_project := project;
 		
-		gtk_new (window); -- create the main window (where pointer "window" is pointing at)
+		-- Set the current project name:
+		log (text => "active project " & enclose_in_quotes (to_string (project)), 
+			 level => log_threshold);
+
+		current_active_project := project;
+
+		-- create the main window (where pointer "window" is pointing at)
+		log (text => "create window", level => log_threshold + 1);
+		gtk_new (window); 
 
 		-- Show the module name in the title bar:
+		log (text => "set title bar", level => log_threshold + 1);
 		set_title_bar (pac_generic_modules.key (module));
-		
+
+		-- Set the window default size:
+		log (text => "set window default size", level => log_threshold + 1);
 		window.set_default_size (1200, 600);
 
 		-- If the operator wishes to terminate the program (by clicking X)
@@ -71,15 +80,22 @@ package body et_gui is
 
 		-- For reaction to keys pressed on the keyboard:
 		window.on_key_press_event (on_key_event'access);
-		
+
+
+		log (text => "build background boxes", level => log_threshold + 1);
 		build_background_boxes;
 
+		log (text => "build primary tool display", level => log_threshold + 1);
 		build_primary_tool_display;
 
+		log (text => "build sheet number display", level => log_threshold + 1);
 		build_sheet_number_display;
-		
+
+		log (text => "build coordinates display", level => log_threshold + 1);
 		build_coordinates_display;
 
+		
+		log (text => "build mode display", level => log_threshold + 1);
 		build_mode_display;
 
 		
@@ -89,27 +105,35 @@ package body et_gui is
 
 		gtk_entry (grid_x.get_child).on_activate (set_grid_x'access);
 		gtk_entry (grid_y.get_child).on_activate (set_grid_y'access);
-		
+
+
+		log (text => "build toolbars", level => log_threshold + 1);
 		build_toolbars;
-		
+
+		log (text => "build console", level => log_threshold + 1);
 		build_console;
 		set_label_console;
 		
 		-- Connect to the on_activate signal of the entry (which is a child of console):
 		gtk_entry (console.get_child).on_activate (execute_command'access); -- on hitting enter
 		
-
+		log (text => "build canvas", level => log_threshold + 1);
 		build_canvas;
 		gtk_new (canvas);
 		
 		-- set the module to be opened and optionally the sheet to be displayed:
+		log (text => "init drawing", level => log_threshold + 1);
 		init_drawing (module, sheet);
 
-		add (scrolled, canvas); -- place the canvas in the scrolled window
-		
+		-- place the canvas in the scrolled window:
+		log (text => "add canvas to scrolled window", level => log_threshold + 1);
+		add (scrolled, canvas);
+
+		log (text => "scale to fit", level => log_threshold + 1);
 		scale_to_fit (canvas);
 
 		-- display the schematic:
+		log (text => "show schematic window", level => log_threshold + 1);
 		window.show_all;
 
 		update_sheet_number_display;
@@ -121,8 +145,8 @@ package body et_gui is
 	procedure init_board (
 		project			: in pac_project_name.bounded_string;	-- blood_sample_analyzer
 		module			: in pac_generic_modules.cursor; -- cursor of generic module to be edited
-		log_threshold_in: in type_log_level) is
-
+		log_threshold_in: in type_log_level) 
+	is
 		use et_canvas_board;
 		use et_canvas_board.pac_canvas;
 		
@@ -131,6 +155,7 @@ package body et_gui is
 		-- using the gui wide variable log_threshold:
 		pac_canvas.log_threshold := log_threshold_in;
 
+		log (text => "create window", level => log_threshold + 1);
 		gtk_new (window); -- create the main window (where pointer "window" is pointing at)
 
 		-- Show the module name in the title bar:
@@ -190,8 +215,8 @@ package body et_gui is
 		module			: in pac_generic_modules.cursor;				-- cursor of generic module
 		sheet			: in et_coordinates.type_sheet := et_coordinates.type_sheet'first; -- the sheet to be opened
 		script			: in pac_script_name.bounded_string; -- rename_nets.scr
-		log_threshold	: in type_log_level) is
-
+		log_threshold	: in type_log_level) 
+	is
 		use et_coordinates;
 	begin
 		log (text => row_separator_single, level => log_threshold);
@@ -205,14 +230,18 @@ package body et_gui is
 			log (text => "script " & enclose_in_quotes (to_string (script)), level => log_threshold);
 		end if;
 
-		gtk.main.init; -- initialize the main gtk stuff
+		-- initialize the main gtk stuff
+		log (text => "init gtk main ... ", level => log_threshold);
+		gtk.main.init;
 
 		-- Set up the schematic window.
+		log (text => "init schematic window ... ", level => log_threshold);
 		init_schematic (project, module, sheet, log_threshold + 1);
 
 		-- CS test if board available (see et_schematic.type_module)
 		
 		-- Set up the board window.
+		log (text => "init board layout window ... ", level => log_threshold);
 		init_board (project, module, log_threshold + 1);
 
 		-- CS
