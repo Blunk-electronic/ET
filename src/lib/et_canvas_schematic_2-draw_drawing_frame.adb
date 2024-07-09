deb240535fb1c512d388880b311e17ac42035711
Mario Blunk <mario.blunk@blunk-electronic.de>
2024-07-09 10:28:02 +0200

    draw outer frame border in schematic clean up

--- src/lib/et_canvas_schematic_2-draw_drawing_frame.adb
+++ src/lib/et_canvas_schematic_2-draw_drawing_frame.adb
@@ -44,55 +44,66 @@ separate (et_canvas_schematic_2)
 procedure draw_drawing_frame is
 	use et_frames;
 
+	-- Get the frames of the schematic:
 	f : type_frames_schematic := element (current_active_module).frames;
+	-- CS use query_element instead
+
+	-- This is a temporarily line that is used to
+	-- draw all the individual lines of the frame:
 	l : pac_geometry_2.type_line;
 
+
+	-- This procedure draws the outer border of the frame:
 	procedure draw_border is
+		-- Get the width of the frame:
 		w : constant type_distance_model_positive := 
 			type_distance_model_positive (f.frame.size.x);
 
+		-- Get the height of the frame:
 		h : constant type_distance_model_positive := 
 			type_distance_model_positive (f.frame.size.y);
 
+
+		-- Draws the temporarily line. Assumes that the 
+		-- lower-left corner of the frame is at (0;0) as it
+		-- is custom for schematic frames:
 		procedure draw_line is begin
 			-- The width of 0.0 has no meaning because 
 			-- the argument do_stroke is false by default
 			-- (see specs of draw_line):
 			draw_line (
 				line		=> l,
-				pos			=> origin_zero_rotation,
+				pos			=> origin_zero_rotation, -- lower-left corner
 				width		=> 0.0);
-
 		end draw_line;
 		
 	begin
+		set_color_frame;
 		set_linewidth (linewidth_2);
-		
+
+		-- Assemble the lower line:
 		l.start_point := (0.0, 0.0);
 		l.end_point := (w, 0.0);
-
 		draw_line;
 
+		-- Assemble the right line:
 		l.start_point := (w, 0.0);
 		l.end_point := (w, h);
-
 		draw_line;
 
-
+		-- Assemble the upper line:
 		l.start_point := (w, h);
 		l.end_point := (0.0, h);
-
 		draw_line;
 
-
+		-- Assemble the left line:
 		l.start_point := (0.0, h);
 		l.end_point := (0.0, 0.0);
-
 		draw_line;
 
 		stroke;
-		
 	end draw_border;
+
 	
 -- 	procedure draw_additional_placeholders is
 -- 		
