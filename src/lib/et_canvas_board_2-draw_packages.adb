4097fe9afb3669a0cca2c90f46594c47793637bf
Mario Blunk <mario.blunk@blunk-electronic.de>
2025-01-24 11:12:10 +0100

    simplifiy code

--- src/lib/et_canvas_board_2-draw_packages.adb
+++ src/lib/et_canvas_board_2-draw_packages.adb
@@ -398,11 +398,9 @@ is
 
 				
 				procedure query_text (c : in pac_doc_texts.cursor) is 
-					text : type_doc_text renames element (c);
 					use pac_draw_text;
 				begin
-					draw_origin ((text.position.place, zero_rotation));
-					draw_vector_text (text.vectors);
+					draw_vector_text_2 (element (c));
 				end query_text;
 
 				
