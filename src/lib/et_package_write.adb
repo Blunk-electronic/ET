495e156556e6615761a1f93b0c70e83fbb8efe8e
Mario Blunk <mario.blunk@blunk-electronic.de>
2026-01-09 11:04:47 +0100

    move write route restrict to separate package

--- src/lib/et_package_write.adb
+++ src/lib/et_package_write.adb
@@ -77,6 +77,7 @@ with et_package_write_assy_doc;			use et_package_write_assy_doc;
 with et_package_write_keepout;			use et_package_write_keepout;
 with et_package_write_stopmask;			use et_package_write_stopmask;
 with et_package_write_stencil;			use et_package_write_stencil;
+with et_package_write_route_restrict;	use et_package_write_route_restrict;
 
 
 package body et_package_write is
@@ -172,76 +173,6 @@ package body et_package_write is
 
 
 		
-		procedure write_route_restrict is 
-			use pac_route_restrict_lines;
-			use pac_route_restrict_arcs;
-			use pac_route_restrict_circles;
-			use pac_route_restrict_zones;
-			use pac_route_restrict_cutouts;
-
-			procedure write_line (cursor : in pac_route_restrict_lines.cursor) is 
-			begin
-				line_begin;
-				write_line (element (cursor));
-				line_end;
-			end write_line;
-
-			procedure write_arc (cursor : in pac_route_restrict_arcs.cursor) is 
-			begin
-				arc_begin;
-				write_arc (element (cursor));
-				arc_end;
-			end write_arc;
-
-			procedure write_circle (cursor : in pac_route_restrict_circles.cursor) is 
-			begin
-				circle_begin;
-				write_circle (element (cursor));
-				circle_end;
-			end write_circle;
-			
-			procedure write_zone (cursor : in pac_route_restrict_zones.cursor) is 
-			begin
-				fill_zone_begin;
-				contours_begin;
-				write_polygon_segments (element (cursor));
-				contours_end;
-				fill_zone_end;
-			end write_zone;
-
-			procedure write_cutout (cursor : in pac_route_restrict_cutouts.cursor) is 
-			begin
-				cutout_zone_begin;
-				contours_begin;
-				write_polygon_segments (element (cursor));
-				contours_end;
-				cutout_zone_end;
-			end write_cutout;
-			
-		begin
-			section_mark (section_route_restrict, HEADER);
-
-			-- top
-			section_mark (section_top, HEADER);
-			iterate (packge.route_restrict.top.lines, write_line'access);
-			iterate (packge.route_restrict.top.arcs, write_arc'access);
-			iterate (packge.route_restrict.top.circles, write_circle'access);
-			iterate (packge.route_restrict.top.zones, write_zone'access);
-			iterate (packge.route_restrict.top.cutouts, write_cutout'access);
-			section_mark (section_top, FOOTER);
-
-			-- bottom
-			section_mark (section_bottom, HEADER);
-			iterate (packge.route_restrict.bottom.lines, write_line'access);
-			iterate (packge.route_restrict.bottom.arcs, write_arc'access);
-			iterate (packge.route_restrict.bottom.circles, write_circle'access);
-			iterate (packge.route_restrict.bottom.zones, write_zone'access);
-			iterate (packge.route_restrict.bottom.cutouts, write_cutout'access);
-			section_mark (section_bottom, FOOTER);
-			
-			section_mark (section_route_restrict, FOOTER);			
-		end write_route_restrict;
-
 		
 		procedure write_via_restrict is 
 			use pac_via_restrict_zones;
@@ -567,7 +498,7 @@ package body et_package_write is
 		write_stopmask (packge, log_threshold + 1);
 		write_stencil (packge, log_threshold + 1);
 		
-		write_route_restrict;
+		write_route_restrict (packge, log_threshold + 1);
 		write_via_restrict;
 		write_holes; -- pcb cutouts
 		-- write_contour_plated; -- pcb contour -- CS currently no need
