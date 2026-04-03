6b730771a2917f5d5137a0e34621453ec674b759
Mario Blunk <mario.blunk@blunk-electronic.de>
2026-04-03 10:38:42 +0200

    comments on things to do

--- src/lib/et_sheets.ads
+++ src/lib/et_sheets.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                               S p e c                                    --
 --                                                                          --
--- Copyright (C) 2017 - 2025                                                --
+-- Copyright (C) 2017 - 2026                                                --
 -- Mario Blunk / Blunk electronic                                           --
 -- Buchfinkenweg 3 / 99097 Erfurt / Germany                                 --
 --                                                                          --
@@ -34,6 +34,9 @@
 --   or visit <http://www.blunk-electronic.de> for more contact data
 --
 --   history of changes:
+--
+-- To Do:
+-- - rename this package to et_sheet_numbers
 --
 
 with ada.containers;    		        use ada.containers;
