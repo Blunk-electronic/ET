6c5ca93ac696af10c492f6e7c809ca83aa0322a0
Mario Blunk <mario.blunk@blunk-electronic.de>
2026-09-01 12:16:21 +0200

    Improve description of procedure copy_group_to_clipboard

--- src/lib/et_schematic_ops_groups.ads
+++ src/lib/et_schematic_ops_groups.ads
@@ -154,8 +154,9 @@ package et_schematic_ops_groups is
 	-- into the clipboard.
 	-- 1. If auto_center is true, then
 	--    the center of the group is used as reference point.
+	--    The argument reference_point is then ignored.
 	-- 2. If auto_center is false, then the explicitly given
-	--    reference_point is used.
+	--    reference_point is used. 
 	-- 3. The x/y component of the global group_reference_point
 	--    is set according to the specified reference_point
 	--    or as the auto genereated center of the group.
