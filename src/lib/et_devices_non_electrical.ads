aa6a3115dcb66a14b153d2ebfaba35e9875e350b
Mario Blunk <mario.blunk@blunk-electronic.de>
2026-09-04 11:13:26 +0200

    comment on procedures to be renamed

--- src/lib/et_devices_non_electrical.ads
+++ src/lib/et_devices_non_electrical.ads
@@ -211,7 +211,7 @@ package et_devices_non_electrical is
 
 
 	-- Moves the device by the given offset:
-	procedure set_place_relative (
+	procedure set_place_relative ( -- CS rename to move_device_by ?
 		device	: in out type_device_non_electrical;
 		offset	: in type_vector_model);
 
