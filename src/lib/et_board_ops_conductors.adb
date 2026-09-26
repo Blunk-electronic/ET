1a2eebc2c1d755c08b3dc86ca4f65ead5086c836
Mario Blunk <mario.blunk@blunk-electronic.de>
2026-09-26 09:48:23 +0200

    prepare procedure copy_selected_conductors

--- src/lib/et_board_ops_conductors.adb
+++ src/lib/et_board_ops_conductors.adb
@@ -6152,6 +6152,10 @@ package body et_board_ops_conductors is
 	is
 		use et_nets;
 		use pac_nets;
+
+		-- In the course of this procedure selected
+		-- conductor objects are searched for. Once an object
+		-- has been found, this flag is set:
 		segment_found : boolean := false;
 
 		-- Here we store the selected segment
@@ -6994,15 +6998,261 @@ package body et_board_ops_conductors is
 
 
 
+
 	procedure copy_selected_conductors (
 		module_cursor	: in pac_generic_modules.cursor;
 		offset			: in type_vector_model; -- x/y
 		log_threshold	: in type_log_level)
 	is
+		-- In the course of this procedure selected
+		-- conductor objects are searched for. Once an object
+		-- has been found, this flag is set:
+		segment_found : boolean := false;
+
+		-- Here we store the selected segment
+		-- of conductor tracks and freetracks::
+		object_line_net : type_object_line_net;
+		object_arc_net : type_object_arc_net;
+
+		object_line_floating : type_object_line_floating;
+		object_arc_floating : type_object_arc_floating;
+		-- CS object_circle_floating : type_object_circle_floating;
+
+
+		procedure query_module (
+			module_name	: in type_module_name;
+			module		: in out type_generic_module)
+		is
+			pragma unreferenced (module_name);
+
+
+			procedure query_nets is
+				use et_nets;
+				use pac_nets;
+				net_cursor : pac_nets.cursor := module.nets.first;
+
+
+				-- This procedure sets the segments of nets
+				-- as moving:
+				procedure query_net (
+					net_name	: in type_net_name;
+					net			: in out type_net)
+				is
+					use et_route;
+					route : type_net_route renames net.route;
+
+					use pac_conductor_lines;
+					line_cursor : pac_conductor_lines.cursor :=
+						route.lines.first;
+
+					use pac_conductor_arcs;
+					arc_cursor : pac_conductor_arcs.cursor :=
+						route.arcs.first;
+
+
+					procedure query_line (
+						line : in out type_track_line)
+					is begin
+						log (text => "line " & to_string (line),
+							level => log_threshold + 3);
+
+					end query_line;
+
+
+					procedure query_arc (
+						arc : in out type_track_arc)
+					is begin
+						null;
+						-- CS: Set arc end (A/B) moving ?
+					end query_arc;
+
+
+				begin
+					log (text => "net " & to_string (net_name),
+						level => log_threshold + 2);
+
+					log_indentation_up;
+
+					-- Iterate through the line track segments.
+					-- Abort once a selected object has been found:
+					while has_element (line_cursor) and not segment_found loop
+						route.lines.update_element (line_cursor, query_line'access);
+						next (line_cursor);
+					end loop;
+
+					-- Iterate through the arc track segments.
+					-- Abort once a selected object has been found:
+					while has_element (arc_cursor) and not segment_found loop
+						route.arcs.update_element (arc_cursor, query_arc'access);
+						next (arc_cursor);
+					end loop;
+
+					-- NOTE: Nets do not have circular conductor segments.
+
+					-- CS: fill zone segments
+
+					log_indentation_down;
+				end query_net;
+
+
+			begin
+				log (text => "segments of nets", level => log_threshold + 1);
+				log_indentation_up;
+
+				-- Iterate through the nets.
+				-- Abort once a selected object has been found:
+				while has_element (net_cursor) and not segment_found loop
+					module.nets.update_element (net_cursor, query_net'access);
+					next (net_cursor);
+				end loop;
+
+				log_indentation_down;
+			end query_nets;
+
+
+
+			-- This procedure sets the segments of freetracks
+			-- as moving:
+			procedure query_freetracks is
+				conductors : type_conductors_floating renames
+					module.board.conductors_floating;
+
+				use pac_conductor_lines;
+				line_cursor : pac_conductor_lines.cursor :=
+					conductors.lines.first;
+
+				use pac_conductor_arcs;
+				arc_cursor : pac_conductor_arcs.cursor :=
+					conductors.arcs.first;
+
+				use pac_conductor_circles;
+				circle_cursor : pac_conductor_circles.cursor :=
+					conductors.circles.first;
+
+
+				procedure query_line (
+					line : in out type_track_line)
+				is begin
+					log (text => "line " & to_string (line),
+						level => log_threshold + 2);
+
+				end query_line;
+
+
+				procedure query_arc (
+					arc : in out type_track_arc)
+				is begin
+					null;
+					-- CS: Set arc end (A/B) moving ?
+				end query_arc;
+
+
+				procedure query_circle (
+					circle : in out type_track_circle)
+				is begin
+					null; -- CS
+				end query_circle;
+
+
+			begin
+				log (text => "freetracks", level => log_threshold + 1);
+				log_indentation_up;
+
+				-- Iterate though the lines.
+				-- Abort once a selected object has been found:
+				while has_element (line_cursor) and not segment_found loop
+					conductors.lines.update_element (line_cursor, query_line'access);
+					next (line_cursor);
+				end loop;
+
+				-- Iterate though the arcs.
+				-- Abort once a selected object has been found:
+				while has_element (arc_cursor) and not segment_found loop
+					conductors.arcs.update_element (arc_cursor, query_arc'access);
+					next (arc_cursor);
+				end loop;
+
+				-- Iterate though the circles.
+				-- Abort once a selected object has been found:
+				while has_element (circle_cursor) and not segment_found loop
+					conductors.circles.update_element (circle_cursor, query_circle'access);
+					next (circle_cursor);
+				end loop;
+
+				-- CS fill zone segments
+
+				log_indentation_down;
+			end query_freetracks;
+
+
+			-- This procedure sets the texts as moving:
+			procedure query_texts is
+				use pac_conductor_texts_board;
+			begin
+				log (text => "texts", level => log_threshold + 1);
+				log_indentation_up;
+				-- CS
+				log_indentation_down;
+			end query_texts;
+
+
+			-- This procedure sets the text placeholders as moving:
+			procedure query_placeholders is
+				use pac_placeholders_conductor;
+			begin
+				log (text => "text placeholders", level => log_threshold + 1);
+				log_indentation_up;
+				-- CS
+				log_indentation_down;
+			end query_placeholders;
+
+
+		begin
+			query_nets;
+			query_freetracks;
+			query_texts;
+			query_placeholders;
+		end query_module;
+
+
 	begin
-		null;
-		-- CS
+		log (text => "module " & to_string (module_cursor)
+			& " copy selected conductors by offset "
+			& to_string (offset),
+			level => log_threshold);
+
+		log_indentation_up;
+
+		-- Search for the first selected object in the group.
+		-- Each object that has been found, will be deselected:
+		generic_modules.update_element (
+			module_cursor, query_module'access);
+
+		-- If an object has been found, then the
+		-- flag "segment_found" is set.
+		-- This starts the following loop where
+		-- the affected object will be copied.
+
+		-- This loop will be executed as long as selected
+		-- objects exist:
+		while segment_found loop
+		-- CS: safety measure to avoid forever-loop
+		-- CS: log the number of segments copied
+
+
+			-- Restart the search for a selected segment:
+			segment_found := false;
+
+			-- generic_modules.update_element (
+			-- 	module_cursor, query_module'access);
+		end loop;
+
+
+		log_indentation_down;
 	end copy_selected_conductors;
+
+
+
 
 
 -----------------------------------------------------------------------------
